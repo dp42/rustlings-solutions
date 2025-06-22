63f11e982e9d1cb4e2aae36472077913cfbe72d7
Koichi SATO <dp42@users.noreply.github.com>
2025-06-23 00:25:42 +0900

    quiz, 12 and 13

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,25 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+
+        for element in input {
+            match element.1 {
+                Command::Uppercase  => {
+                    output.push(element.0.to_uppercase());
+                }
+                Command::Trim  => {
+                    output.push(element.0.trim().to_string());
+                }
+                Command::Append(count) => {
+                    output.push(element.0 + &"bar".repeat(count));
+                }
+            }
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -39,6 +58,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
